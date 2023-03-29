f6adac19818986bf1eb79b00b93ef089b2c4b5d0
Kuldeep <kuldeep2code@gmail.com>
2023-03-29 17:36:17 +0530

    modified

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-var app = require( './app' );
-console.log('hello' , app);
+// var app = require( './app' );
+// console.log('hello' , app);
 
-const arr = [4,5,2,6,74,52,21,0,12,2]
-let res = arr.filter((item)=>{
-    return item>22
-});
+// const arr = [4,5,2,6,74,52,21,0,12,2]
+// let res = arr.filter((item)=>{
+//     return item>22
+// });
 
-console.warn(res);
+// console.warn(res);
 
-// modules -> global and non - global
+// // modules -> global and non - global
 
-console.log('This is global module');
-var fs = require('fs'); //fs is non global
-fs.writeFileSync('temp.txt' , 'This is temp file');
+// console.log('This is global module');
+// var fs = require('fs'); //fs is non global
+// fs.writeFileSync('temp.txt' , 'This is temp file');
+
+require('colors');
+console.log("kuldeep".bgCyan.blue)
